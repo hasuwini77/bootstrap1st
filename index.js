233866f669d80ac082f96a6256cd41ff30822a85
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-08 22:44:36 +0100

    local storage added on theme dark and light

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const modeButton = document.querySelector("img.dark-light");
 const htmlElem = document.querySelector("html");
+
+// Retrieve the saved theme from local storage, or default to 'light'
+const savedTheme = localStorage.getItem("theme") || "light";
+htmlElem.setAttribute("data-bs-theme", savedTheme);
+
+// Function to update theme and store in local storage
+const updateTheme = (newTheme) => {
+  htmlElem.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+};
+
 const onClickFunction = () => {
   let currentTheme = htmlElem.getAttribute("data-bs-theme");
   let myTheme = currentTheme === "light" ? "dark" : "light";
-  htmlElem.setAttribute("data-bs-theme", myTheme);
+
+  // Update the theme and store it in local storage
+  updateTheme(myTheme);
+
   if (currentTheme === "dark") {
     modeButton.src = "./sunrise-fill.svg";
     modeButton.alt = "dark cloud";
