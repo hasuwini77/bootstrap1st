97c67a44df36e1433db145a39cb0fa3f2ef205cd
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-08 22:26:03 +0100

    dark mode logic added with .src and .alt on modeButton

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const modeButton = document.querySelector(".dark-light");
+const modeButton = document.querySelector("img.dark-light");
 const htmlElem = document.querySelector("html");
 const onClickFunction = () => {
   let currentTheme = htmlElem.getAttribute("data-bs-theme");
   let myTheme = currentTheme === "light" ? "dark" : "light";
   htmlElem.setAttribute("data-bs-theme", myTheme);
+  if (currentTheme === "dark") {
+    modeButton.src = "./sunrise-fill.svg";
+    modeButton.alt = "dark cloud";
+  } else {
+    modeButton.src = "./cloud-moon-fill.svg";
+    modeButton.alt = "light cloud";
+  }
 };
 
 modeButton.addEventListener("click", onClickFunction);
